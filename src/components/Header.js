a34d546f693d55d2c6bb41aaaced073f11e25f4b
Joseph Murray <70991796+asparageist@users.noreply.github.com>
2024-01-08 19:04:54 -0800

    lesson up to Adding Local State

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import mushImage from "./../img/fly-mush.webp"
+
+function Header(){
+  return (
+    <React.Fragment>
+    <h1>Help Queue</h1>
+    <h3>get ye some help</h3>
+    <img src={mushImage} alt="An image of fly amanita" />
+
+    </React.Fragment>
+  );
+}
+
+export default Header;
+
---
+++ src/components/TicketControl.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NewTicketForm from './NewTicketForm';
+import TicketList from './TicketList';
+
+class TicketControl extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <React.Fragment>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default TicketControl;
+
